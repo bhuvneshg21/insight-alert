8553fed9875ed68845121831deeb59d4227abf33
bhuvneshg21 <bhuvneshgupta21@hotmail.com>
2024-11-14 14:57:20 +0530

    home

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Link , useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './HomePage.css';
 import { customersData, insightAlertBoxData } from './data';
 import InsightAlertIcon from './assets/InsightAlertSmall.png';
 import lily from './assets/lily.png';
 import bubble from './assets/bubble.png';
-import cashgrail from './assets/cashgrail.png';  
+import cashgrail from './assets/cashgrail.png';
 function HomePage() {
   const [showFunctionalitiesBar, setShowFunctionalitiesBar] = useState(false);
   const [popoverData, setPopoverData] = useState(null);
@@ -58,7 +58,7 @@ function HomePage() {
 
   return (
     <div className="sales-os">
-         <header className="header">
+      <header className="header">
         <div className="header-left">
           <div className="logo">Sales<sup>+</sup></div>
           <div className="search-container">
@@ -82,6 +82,7 @@ function HomePage() {
 
       <div className="content">
         {/* Left Sidebar - 25% */}
+     
         <div className="left-sidebar">
           <h2>Recent Updates Feed</h2>
           <ul className="updates-list">
@@ -92,11 +93,12 @@ function HomePage() {
           <Link to="#" className="manage-link">Manage Target Accounts</Link>
 
           <div className="insight-alert-box">
-            <h3>
+          <div className="insight-alert-header">
               <img src={InsightAlertIcon} alt="Insight Alert Icon" className="insight-alert-icon" />
-              Insight Alerts!
-            </h3>
-            <p>Actionable Data for Optimal Results</p>
+              <h3>Insight Alerts!</h3>
+            </div>
+            <p className="alert-subtext"><strong>Actionable Data for Optimal Results</strong></p>
+            
             <button
               className="view-dashboard-link"
               onClick={() => navigate(`/insight-alert/${selectedCustomer.id}`)}
@@ -104,20 +106,20 @@ function HomePage() {
               View Dashboard
             </button>
             <h4>Usage Data</h4>
-          <ul>
-            <li>You have generated 120 CoPilot-driven contact searches this week.</li>
-            <li>You have exported 75 contact data.</li>
-            <li>You have successfully set up your Domain under Websight to track companies visiting your company.</li>
-          </ul>
-          <h4>Did You Know?</h4>
-          <ul>
-            <li>Based on the exports to your CRM over the past 8 weeks, we have identified that 40% of your data requires updates.</li>
-            <li>According to your Target account records, 9 companies have visited your Websight in the past two weeks.</li>
-          </ul>
-          <h4>Unexplored Features</h4>
-          <p>You have not set up the Intent topics.</p>
+            <ul>
+              <li>You have generated 120 CoPilot-driven contact searches this week.</li>
+              <li>You have exported 75 contact data.</li>
+              <li>You have successfully set up your Domain under Websight to track companies visiting your company.</li>
+            </ul>
+            <h4>Did You Know?</h4>
+            <ul>
+              <li>Based on the exports to your CRM over the past 8 weeks, we have identified that 40% of your data requires updates.</li>
+              <li>According to your Target account records, 9 companies have visited your Websight in the past two weeks.</li>
+            </ul>
+            <h4>Unexplored Features</h4>
+            <p>You have not set up the Intent topics.</p>
           </div>
-          
+
 
           <div className="need-help-box">
             <h3>Need Help?</h3>
@@ -145,77 +147,77 @@ function HomePage() {
 
         {/* Middle Section - 50% */}
         {/* <main className="middle-content"> */}
-        <div className="main-content"> 
+        <div className="main-content">
           {/* Top Updates Box */}
-        <div className="top-updates-box">
-          <h2>Top updates for today <span className="powered-by-ai">Powered by AI</span></h2>
-          <p className="updates-description">Prioritized by signal strength and recency</p>
-          <button className="learn-more-btn">Learn More</button>
-        </div>
-         {/* Eli Lilly Update Box */}
-         <div className="update-card">
-          <div className="update-header">
-            <img src={lily} alt="icon" className="company-icon" />
-            <h3>Eli Lilly <button className="ask-copilot-btn">Ask Copilot</button></h3>
+          <div className="top-updates-box">
+            <h2>Top updates for today <span className="powered-by-ai">Powered by AI</span></h2>
+            <p className="updates-description">Prioritized by signal strength and recency</p>
+            <button className="learn-more-btn">Learn More</button>
           </div>
-          <div className="update-details">
-            <span className="badge">Great Account Fit</span>
-            <span className="badge">Account in Salesforce</span>
-            <span className="badge">Target Account</span>
-            <p className="description">Eli Lilly is a pharmaceutical company with over 10K employees that develops and manufactures a range of healthcare products...</p>
+          {/* Eli Lilly Update Box */}
+          <div className="update-card">
+            <div className="update-header">
+              <img src={lily} alt="icon" className="company-icon" />
+              <h3>Eli Lilly <button className="ask-copilot-btn">Ask Copilot</button></h3>
+            </div>
+            <div className="update-details">
+              <span className="badge">Great Account Fit</span>
+              <span className="badge">Account in Salesforce</span>
+              <span className="badge">Target Account</span>
+              <p className="description">Eli Lilly is a pharmaceutical company with over 10K employees that develops and manufactures a range of healthcare products...</p>
+            </div>
+            <div className="recent-signals">
+              <h4>Recent signals</h4>
+              <p><strong>Last 30 days</strong></p>
+              <ul>
+                <li><a href="#">Hiring Plans:</a> Eli Lilly has more open roles in Engineering & Technical...</li>
+                <li><a href="#">Partnership:</a> Eli Lilly has joined the Workforce Council...</li>
+              </ul>
+            </div>
+            <button className="find-group-btn">Reach out to potential Buying Group members</button>
           </div>
-          <div className="recent-signals">
-            <h4>Recent signals</h4>
-            <p><strong>Last 30 days</strong></p>
-            <ul>
-              <li><a href="#">Hiring Plans:</a> Eli Lilly has more open roles in Engineering & Technical...</li>
-              <li><a href="#">Partnership:</a> Eli Lilly has joined the Workforce Council...</li>
-            </ul>
+          <div className="update-card">
+            <div className="update-header">
+              <img src={lily} alt="icon" className="company-icon" />
+              <h3>Eli Lilly <button className="ask-copilot-btn">Ask Copilot</button></h3>
+            </div>
+            <div className="update-details">
+              <span className="badge">Great Account Fit</span>
+              <span className="badge">Account in Salesforce</span>
+              <span className="badge">Target Account</span>
+              <p className="description">Eli Lilly is a pharmaceutical company with over 10K employees that develops and manufactures a range of healthcare products...</p>
+            </div>
+            <div className="recent-signals">
+              <h4>Recent signals</h4>
+              <p><strong>Last 30 days</strong></p>
+              <ul>
+                <li><a href="#">Hiring Plans:</a> Eli Lilly has more open roles in Engineering & Technical...</li>
+                <li><a href="#">Partnership:</a> Eli Lilly has joined the Workforce Council...</li>
+              </ul>
+            </div>
+            <button className="find-group-btn">Reach out to potential Buying Group members</button>
           </div>
-          <button className="find-group-btn">Reach out to potential Buying Group members</button>
-        </div>
-        <div className="update-card">
-          <div className="update-header">
-            <img src={lily} alt="icon" className="company-icon" />
-            <h3>Eli Lilly <button className="ask-copilot-btn">Ask Copilot</button></h3>
+          <div className="update-card">
+            <div className="update-header">
+              <img src={lily} alt="icon" className="company-icon" />
+              <h3>Eli Lilly <button className="ask-copilot-btn">Ask Copilot</button></h3>
+            </div>
+            <div className="update-details">
+              <span className="badge">Great Account Fit</span>
+              <span className="badge">Account in Salesforce</span>
+              <span className="badge">Target Account</span>
+              <p className="description">Eli Lilly is a pharmaceutical company with over 10K employees that develops and manufactures a range of healthcare products...</p>
+            </div>
+            <div className="recent-signals">
+              <h4>Recent signals</h4>
+              <p><strong>Last 30 days</strong></p>
+              <ul>
+                <li><a href="#">Hiring Plans:</a> Eli Lilly has more open roles in Engineering & Technical...</li>
+                <li><a href="#">Partnership:</a> Eli Lilly has joined the Workforce Council...</li>
+              </ul>
+            </div>
+            <button className="find-group-btn">Reach out to potential Buying Group members</button>
           </div>
-          <div className="update-details">
-            <span className="badge">Great Account Fit</span>
-            <span className="badge">Account in Salesforce</span>
-            <span className="badge">Target Account</span>
-            <p className="description">Eli Lilly is a pharmaceutical company with over 10K employees that develops and manufactures a range of healthcare products...</p>
-          </div>
-          <div className="recent-signals">
-            <h4>Recent signals</h4>
-            <p><strong>Last 30 days</strong></p>
-            <ul>
-              <li><a href="#">Hiring Plans:</a> Eli Lilly has more open roles in Engineering & Technical...</li>
-              <li><a href="#">Partnership:</a> Eli Lilly has joined the Workforce Council...</li>
-            </ul>
-          </div>
-          <button className="find-group-btn">Reach out to potential Buying Group members</button>
-        </div>
-        <div className="update-card">
-          <div className="update-header">
-            <img src={lily} alt="icon" className="company-icon" />
-            <h3>Eli Lilly <button className="ask-copilot-btn">Ask Copilot</button></h3>
-          </div>
-          <div className="update-details">
-            <span className="badge">Great Account Fit</span>
-            <span className="badge">Account in Salesforce</span>
-            <span className="badge">Target Account</span>
-            <p className="description">Eli Lilly is a pharmaceutical company with over 10K employees that develops and manufactures a range of healthcare products...</p>
-          </div>
-          <div className="recent-signals">
-            <h4>Recent signals</h4>
-            <p><strong>Last 30 days</strong></p>
-            <ul>
-              <li><a href="#">Hiring Plans:</a> Eli Lilly has more open roles in Engineering & Technical...</li>
-              <li><a href="#">Partnership:</a> Eli Lilly has joined the Workforce Council...</li>
-            </ul>
-          </div>
-          <button className="find-group-btn">Reach out to potential Buying Group members</button>
-        </div>
           {/* <div className="customer-cards">
             {customersData.map(customer => (
               <div className="customer-card" key={customer.id}>
@@ -230,7 +232,7 @@ function HomePage() {
               </div>
             ))}
           </div> */}
-          </div>
+        </div>
         {/* </main> */}
 
         {/* Right Sidebar - 25% */}
@@ -239,7 +241,7 @@ function HomePage() {
         </div>
 
         {/* Functionalities Bar */}
-        <div className={`functionalities-bar ${showFunctionalitiesBar ? "expanded" : ""}`} onClick={toggleFunctionalitiesBar}>
+        {/* <div className={`functionalities-bar ${showFunctionalitiesBar ? "expanded" : ""}`} onClick={toggleFunctionalitiesBar}>
           <p>{showFunctionalitiesBar ? "Click to see more functionalities" : "70% of functionalities used"}</p>
           {showFunctionalitiesBar && (
             <div className="functionalities-details">
@@ -250,7 +252,7 @@ function HomePage() {
               </ul>
             </div>
           )}
-        </div>
+        </div> */}
       </div>
     </div>
   );
